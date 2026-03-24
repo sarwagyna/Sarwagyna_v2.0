7cfa206df2010ed5b2456af840183d0573c6a475
[firstname <sarwannandhofficial672007@gmail.com>
2026-03-24 21:42:01 +0530

    FINAL CSP ADDITIONS

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
   async headers() {
     return [
       {
@@ -21,17 +22,18 @@ const nextConfig: NextConfig = {
             value: [
               "default-src 'self'",
 
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://core.sanity-cdn.com https://sanity-cdn.com",
-              "script-src-elem 'self' 'unsafe-inline' https://core.sanity-cdn.com https://sanity-cdn.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://core.sanity-cdn.com https://sanity-cdn.com https://www.googletagmanager.com https://www.clarity.ms https://scripts.clarity.ms",
 
-              "connect-src 'self' https://*.api.sanity.io https://*.sanity.io wss://*.api.sanity.io wss://*.sanity.io https://core.sanity-cdn.com https://sanity-cdn.com https://hzvhbnohuiodjhndotpb.supabase.co wss://hzvhbnohuiodjhndotpb.supabase.co",
+              "script-src-elem 'self' 'unsafe-inline' https://core.sanity-cdn.com https://sanity-cdn.com https://www.googletagmanager.com https://www.clarity.ms https://scripts.clarity.ms",
+
+              "connect-src 'self' https://*.api.sanity.io https://*.sanity.io wss://*.api.sanity.io wss://*.sanity.io https://core.sanity-cdn.com https://sanity-cdn.com https://hzvhbnohuiodjhndotpb.supabase.co wss://hzvhbnohuiodjhndotpb.supabase.co https://www.google-analytics.com https://region1.google-analytics.com https://www.clarity.ms https://*.clarity.ms",
 
               "style-src 'self' 'unsafe-inline' https://core.sanity-cdn.com https://sanity-cdn.com https://fonts.googleapis.com",
               "style-src-elem 'self' 'unsafe-inline' https://core.sanity-cdn.com https://sanity-cdn.com https://fonts.googleapis.com",
 
               "font-src 'self' data: https://fonts.gstatic.com https://core.sanity-cdn.com https://sanity-cdn.com",
 
-              "img-src 'self' data: blob: https:",
+              "img-src 'self' data: blob: https: https://www.google-analytics.com",
               "frame-src 'self' https://*.sanity.io",
               "worker-src 'self' blob:",
             ].join("; "),
