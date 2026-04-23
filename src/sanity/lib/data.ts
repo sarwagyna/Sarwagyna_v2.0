ad798c5b7fdffe43a7e719d0279adcbf8cb49da6
[firstname <sarwannandhofficial672007@gmail.com>
2026-04-23 22:40:55 +0530

    fix seo errors

--- src/sanity/lib/data.ts
+++ src/sanity/lib/data.ts
@@ -2,9 +2,9 @@ import { client } from '@/sanity/lib/client'
 import { POSTS_QUERY, POST_QUERY } from './queries'
 
 export async function getAllPosts() {
-  return await client.fetch(POSTS_QUERY, {}, { next: { revalidate: 60 } })
+  return await client.fetch(POSTS_QUERY, {}, { next: { revalidate: 30 } })
 }
 
 export async function getPostBySlug(slug: string) {
-  return await client.fetch(POST_QUERY, { slug }, { next: { revalidate: 60 } })
+  return await client.fetch(POST_QUERY, { slug }, { next: { revalidate: 30 } })
 }
