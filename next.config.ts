72c64314b8334baf9cd7b5ff7faa028994ee0b7e
[firstname <sarwannandhofficial672007@gmail.com>
2026-06-08 12:11:03 +0530

    Next Config Fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   async redirects() {
     return [
+      // Force HTTPS
+      {
+        source: '/:path*',
+        has: [{ type: 'header', key: 'x-forwarded-proto', value: 'http' }],
+        destination: 'https://sarwagyna.com/:path*',
+        permanent: true,
+      },
       // Redirect www to non-www
       {
         source: '/:path*',
